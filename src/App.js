f2ecc80732c0ddcfdadcefc4f1102b9f2b4960ae
Riham Aji <reham93189310@gmail.com>
2023-11-08 19:13:27 +0200

    Fix Duplication

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import About from "./pages/about/about";
 
 function App() {
   return (
-      <>
+      <div>
           <Routes>
               <Route path={'/'} element={<Home/>}>Home</Route>
               <Route path={'/feature'} element={<Feature/>}>feature</Route>
-            <Route path={'/about'} element={<About/>}>About</Route>
+               <Route path={'/about'} element={<About/>}>About</Route>
             {/*    <Route path={'/categories'} element={<Section1/>}>Categories</Route>
               <Route path={'/users'} element={<Section1/>}></Route>*/}
           </Routes>
-         {/*  <Home/>*/}
-      </>
+      </div>
   );
 }
 
